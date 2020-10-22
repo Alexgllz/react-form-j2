9f40ff37804ca8d2982f64038e511f710d0f0ef1
Alexgllz <galliez.alex@hotmail.fr>
2020-10-22 12:00:47 +0200

    update posts
    ajout retour home

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -57128,7 +57128,10 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var Posts = function Posts() {
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, "posts"));
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, "posts"), /*#__PURE__*/_react.default.createElement(_reactRouterDom.Link, {
+    className: "btn btn-primary",
+    to: "/"
+  }, "Retour vers la home"));
 };
 
 exports.Posts = Posts;
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Posts = () => {
-    return (<div><p>posts</p></div>
+    return (<div><p>posts</p>
+    <Link className="btn btn-primary" to="/">
+        Retour vers la home
+      </Link>
+    </div>
     );
 };
